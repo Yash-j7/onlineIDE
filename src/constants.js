ced4b15c5617c3d6ab2e0137c0b96bd64aea4646
Yash Jha <yashjha00000@gmail.com>
2024-12-24 12:45:28 +0530

    fixedBugs

--- src/constants.js
+++ src/constants.js
@@ -8,11 +8,11 @@ export const LANGUAGE_VERSIONS = {
 };
 
 export const CODE_SNIPPETS = {
-  javascript: `\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\ngreet("Alex");\n`,
-  typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "!");\n}\n\ngreet({ name: "Alex" });\n`,
-  python: `\ndef greet(name):\n\tprint("Hello, " + name + "!")\n\ngreet("Alex")\n`,
-  java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}\n`,
+  javascript: `\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "!, Welcome to intelli-Class");\n}\n\ngreet("Yash");\n`,
+  typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "!, Welcome to intelli-Class");\n}\n\ngreet({ name: "Yash" });\n`,
+  python: `\ndef greet(name):\n\tprint("Hello, " + name + "!, Welcome to intelli-Class")\n\ngreet("Yash")\n`,
+  java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World, Welcome to intelli-Class");\n\t}\n}\n`,
   csharp:
-    'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#");\n\t\t}\n\t}\n}\n',
-  php: "<?php\n\n$name = 'Alex';\necho $name;\n",
+    'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#, Welcome to intelli-Class");\n\t\t}\n\t}\n}\n',
+  php: "<?php\n\n$name = 'Yash';\necho $name;\n",
 };
